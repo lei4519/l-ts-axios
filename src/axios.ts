2ac821ced96b17d6af1273cf620881a6925b38fb
李金磊 <leiaiyaliu@gmail.com>
2019-06-22 18:41:18 +0800

    feat(all the file): init project
    init project by typescript library starter

---
+++ src/axios.ts
@@ -0,0 +1,9 @@
+// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
+// import "core-js/fn/array.find"
+// ...
+export default class DummyClass {
+  name: string
+  constructor(name: string) {
+    this.name = name
+  }
+}
