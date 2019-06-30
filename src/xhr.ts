91e232814dd5e68257d4484a002359084d36c5dc
李金磊 <leiaiyaliu@gmail.com>
2019-06-30 21:22:19 +0800

    1

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,7 +1,6 @@
 import { AxiosRequestConfig } from './types'
-
 export default function xhr(config: AxiosRequestConfig): void {
-  const { url = '', data = null, method = 'GET' } = config
+  let { url = '', data = null, method = 'GET' } = config
   const request = new XMLHttpRequest()
   request.open(method.toUpperCase(), url, false)
   request.send(data)
